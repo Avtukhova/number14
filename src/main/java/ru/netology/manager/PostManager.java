e607e404128577b2bb0e0ed800853b439543b5de
Larisa Avtukhova <aln1967@yandex.ru>
2021-08-12 20:21:04 +0300

    Added PostManager.java

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,18 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+        private Post[]search;
+
+
+        public Post[] search() {
+            return null;
+
+        }
+
+        public void delete() {
+
+        }
+
+    }
