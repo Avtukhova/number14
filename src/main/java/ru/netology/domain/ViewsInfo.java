e2cd2a37e489646da8997c2120ca0a1f2e5d7385
Larisa Avtukhova <aln1967@yandex.ru>
2021-08-12 18:07:21 +0300

    Added ViewsInfo.java

---
+++ src/main/java/ru/netology/domain/ViewsInfo.java
@@ -0,0 +1,14 @@
+package ru.netology.domain;
+
+public class ViewsInfo {
+    private int countViews; //количество просмотров
+
+    public int getCountViews() {
+        return countViews;
+    }
+
+    public void setCountViews(int countViews) {
+        this.countViews = countViews;
+    }
+
+}
