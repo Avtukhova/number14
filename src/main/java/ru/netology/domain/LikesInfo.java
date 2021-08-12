c0466f20b53a6e0163465b31011033b4530aceee
Larisa Avtukhova <aln1967@yandex.ru>
2021-08-12 18:05:43 +0300

    Added LikesInfo.java

---
+++ src/main/java/ru/netology/domain/LikesInfo.java
@@ -0,0 +1,14 @@
+package ru.netology.domain;
+
+public class LikesInfo {
+    private int countLikes; //количество лайков
+
+
+    public int getCountlikes() {
+        return countLikes;
+    }
+
+    public void setCountLikes(int countLikes) {
+        this.countLikes = countLikes;
+    }
+}
