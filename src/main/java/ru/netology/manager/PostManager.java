6dfd74d7ff6d9a1bc0e0d9beb253749193bbef6a
Larisa Avtukhova <aln1967@yandex.ru>
2021-08-18 20:24:53 +0300

    Changes PostManager.java

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -5,49 +5,21 @@ import ru.netology.domain.Post;
 public class PostManager {
     private Post[] posts;
 
-    private int idOwner;  // идентификатор пользователя или сообщества
-    private String address; // адрес пользователя или сообщества
-    private String query; // поисковый запрос
-    private boolean idOwnersOnly;  // записи только данного пользователя
-    private int count;  // количество записей, соответствующих запросу
-    private int idPost;  // идентификатор записи
+    private int idOwner;
+    private String address;
+    private String query;
+    private boolean idOwnersOnly;
+    private int count;
+    private int idPost;
 
 
-
-    public Post[] searchPostIdOwner() {
+    public Post[] WallSearch(int idOwner, String address, String query, boolean idOwnersOnly, int count) {
         // add logic
         return null;
     }
 
-    public Post[] searchAddress() {
+    public void WallDelete(int idOwner, int idPost) {
         // add logic
-        return null;
-    }
-
-    public Post[] searchQuery() {
-        // add logic
-        return null;
-    }
-
-    public Post[] searchIdOwnersOnly() {
-        // add logic
-        return null;
-    }
-
-    public Post[] searchCount() {
-        // add logic
-        return null;
-    }
-
-    public Post[] searchIdPost() {
-        // add logic
-        return null;
-    }
-
-    public void deleteIdOwnersOnly() {
-        // add logic
-
     }
 
 }
-
